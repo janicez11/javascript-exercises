95750026508f4382c81664d4d4b590f4c247b46c
Janice Zhao <j5zhao@ucsd.edu>
2022-08-22 14:55:35 -0700

    Complete exercise 3, use array and string methods to reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split("").reverse().toString();
+    array = array.replace(/,/g, "");
+    return array;
 };
 
 // Do not edit below this line
