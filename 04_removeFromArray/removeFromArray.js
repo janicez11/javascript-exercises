1b8efa27a28abc3a0aafd445c1e21d474583b024
Janice Zhao <j5zhao@ucsd.edu>
2022-08-22 15:48:01 -0700

    Complete exercise 4, use double for loop to remove rest parameters from array with splice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray, x, ...moreArgs) {
+    moreArgs.unshift(x);
+    //console.log(moreArgs);
+    
+    for (let i = 0; i < myArray.length; i++) {
+        for(let j = 0; j < moreArgs.length; j++) {
+            if (myArray[i] === moreArgs[j]) {
+                //console.log(myArray[i], moreArgs[j]);
+                myArray.splice(i, 1);
+                i--;
+                //console.log(myArray);
+            }
+        }
+    }
+    return myArray;
 };
 
 // Do not edit below this line
