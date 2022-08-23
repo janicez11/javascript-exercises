6de4ff8712287039492bcd14508054c18c4f7958
Janice Zhao <j5zhao@ucsd.edu>
2022-08-22 20:24:19 -0700

    Complete exercise 7, convert temperature between F and C

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempf) {
+  let tempc = (tempf - 32) * (5/9);
+  tempc = Math.round(tempc * 10) / 10;
+  return tempc
 };
 
-const ctof = function() {
-
+const ctof = function(tempc) {
+  let tempf = (tempc * (9/5) + 32);
+  tempf = Math.round(tempf * 10) / 10;
+  return tempf
 };
 
 // Do not edit below this line
