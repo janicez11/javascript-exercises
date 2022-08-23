697f7bc008329a16f1f625570aeed0bc47c2ddbf
Janice Zhao <j5zhao@ucsd.edu>
2022-08-22 20:15:27 -0700

    Complete exercise 6, identify leap years using conditionals

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,25 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    
 
+    if ( year%4 == 0) {
+        //console.log("divisible by 4")
+       if (year%100 == 0 && year%400 == 0) {
+            //console.log("divisible by both")
+            return true;
+        }
+        else if (year%100 != 0) {
+            return true;
+        }
+        else {
+            return false;
+        }      
+    }
+    else {
+        return false;
+    }
+   
+
+   
 };
 
 // Do not edit below this line
