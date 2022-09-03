d812fb4cd93699c345f50b1e646ac78364151b8f
Janice Zhao <j5zhao@ucsd.edu>
2022-09-02 21:16:51 -0700

    Complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    
+    var regex = /[\s!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
 
+    var cleanString = string.replace(regex, '');
+    cleanString = cleanString.toLowerCase();
+    cleanArray = Array.from(cleanString);
+    
+    arrayMid = Math.floor(cleanArray.length/2);
+
+    for (let i = 0; i < arrayMid; i++) {
+        if (cleanArray[i] != cleanArray[cleanArray.length - (i+1)]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
