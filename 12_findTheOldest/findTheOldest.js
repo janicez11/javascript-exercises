c2946a3dade9c685b25f1f8740ad8944964bff27
Janice Zhao <j5zhao@ucsd.edu>
2022-09-02 21:56:50 -0700

    Complete finding the oldest person in an array of objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(list) {
+    let max = 0;
+    let index = 0;
+    let yearOld;
 
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].yearOfDeath === undefined) {
+            let yearNow = new Date();
+            yearNow = yearNow.getFullYear();
+            //console.log(yearNow);
+            yearOld = yearNow - list[i].yearOfBirth;
+        }
+        else {
+            yearOld = list[i].yearOfDeath - list[i].yearOfBirth;
+        }
+        
+        if (yearOld > max) {
+            max = yearOld;
+            index = i;
+        }
+    }
+    return list[index];
 };
 
 // Do not edit below this line
