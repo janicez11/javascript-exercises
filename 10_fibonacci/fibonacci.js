b7c6dcae7335d07d4d238633c268a7d550d64b07
Janice Zhao <j5zhao@ucsd.edu>
2022-09-02 21:34:07 -0700

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    let array = [1, 1];
+    let sum = 0;
 
+    if (member > 0) {
+        for ( let i = 2; i < member; i++) {
+            sum = array[i-2] + array[i-1];
+            array.push(sum);
+        }
+        return array[member-1];
+    }
+    else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
